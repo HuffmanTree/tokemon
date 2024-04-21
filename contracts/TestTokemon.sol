be29719192cb70866a72e41908abc1b333d1dd6e
François Guérin <francoisguerin1@hotmail.fr>
2024-04-21 17:57:57 +0200

    Add test helpers

--- contracts/TestTokemon.sol
+++ contracts/TestTokemon.sol
@@ -10,7 +10,23 @@ contract TestTokemon is Tokemon {
         _balances[owner] = amount;
     }
 
+    function getBalance(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
     function setOwner(uint256 tokenId, address owner) public {
         _owners[tokenId] = owner;
+    }
+
+    function getOwner(uint256 tokenId) public view returns (address) {
+        return _owners[tokenId];
+    }
+
+    function setTokenApproval(uint256 tokenId, address approval) public {
+        _tokenApprovals[tokenId] = approval;
+    }
+
+    function addOperatorApproval(address owner, address approval) public {
+        _operatorApprovals[owner][approval] = true;
     }
 }
